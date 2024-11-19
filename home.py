c80cd43fce43261529bdf0af8758844d0deeb892
Artzaza159 <Artz167789@gmail.com>
2024-11-19 14:47:54 +0700

    Update home.py

--- home.py
+++ home.py
@@ -4,4 +4,9 @@ import pandas as pd
 st.title("🍌🍌Website Devoloping using python🍌🍌")
 st.header("🍖🍖Website Devoloping using python🍖🍖")
 
+st.image('./img/Raccoon.jpeg')
 st.subheader("Danunai Huadcharoen")
+
+dt=pd.read_csv('./data/iris-3.csv')
+st.header()
+st.write(dt.head(10))
