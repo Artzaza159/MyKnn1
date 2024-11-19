a82d7c35c3e2942a8da4dd3f2ccebc2009210818
Artzaza159 <Artz167789@gmail.com>
2024-11-19 15:48:49 +0700

    Update knn.py

--- knn.py
+++ knn.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.header('kairung')
+st.header('Danunai')
 #st.image("./pic/kairung.jpg")
 col1, col2, col3 = st.columns(3)
 
